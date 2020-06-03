3e1e38b1c0b61c8ce0eebde3005c104f2e797933
Nat Geyzen <nat.geyzen@outlook.com>
2020-06-03 00:46:31 -0500

    all major/known logic glitches inside filter page worked out, minor responsiveness fixes needed for final touches on page

--- name_filter.js
+++ name_filter.js
@@ -8,6 +8,9 @@ const name_filter_2 = document.getElementById('name-filter-2');
 const name_filter_3 = document.getElementById('name-filter-3');
 const name_filter_4 = document.getElementById('name-filter-4');
 
+// ----- array of filter containers ------
+const filters = [name_filter_1, name_filter_2, name_filter_3, name_filter_4];
+
 // ----- filter buttons ------
 const time_button = document.getElementById('time-value');
 const setting_button = document.getElementById('setting-value');
@@ -38,16 +41,13 @@ const space_species_selections = document.getElementById('space-species-selectio
 const techworld_species_selections = document.getElementById('techworld-species-selections');
 const postapoc_species_selections = document.getElementById('postapoc-species-selections');
 
-// ----- array containing any filters when complete ------
-const complete_array = Array.from(document.getElementsByClassName('complete'));
-
 // ----- arrays for each filter selection value ------
 const time_selection = [];
 const setting_selection = [];
 const gender_selection = [];
 const species_selection = [];
 
-
+const checkmark_button = document.getElementById('checkmark');
 
 /* ------------------------------------------------------------------------------------------------------
     FUNCTIONS
@@ -59,113 +59,169 @@ const toggleClass = (element, oldClass, newClass) => {
     element.classList.add(newClass);
 };
 
+// ----- when a filter is marked complete, highlight the next filter (making it 'active') ------
+const scaleInNextFilter = (nextFilter) => {
+    if (nextFilter !== null && !nextFilter.classList.contains('fade-out') && !nextFilter.classList.contains('fade-in')) {
+        toggleClass(nextFilter, 'scaled-out', 'scale-bounce-in');    
+    }
+};
+
+// ----- helper function for next function ------
+const fadeOut = (filter) => {
+    if (filter.classList.contains('scale-bounce-in')) {
+        toggleClass(filter, 'scale-bounce-in', 'fade-out');
+    } else if (filter.classList.contains('fade-in')) {
+        toggleClass(filter, 'fade-in', 'fade-out');
+    }
+    if (filter.classList.contains('list-open')) {
+        toggleClass(filter, 'list-open', 'list-closed');
+    }
+};
+
+// ----- when a filter list is opened, hide the next filters if any are visible ------
+const fadeOutNextFilters = (button) => {
+    const scale_in_filters = Array.from(document.getElementsByClassName('scale-bounce-in'));
+    const fade_in_filters = Array.from(document.getElementsByClassName('fade-in'));
+    const visible_filters = [...scale_in_filters, ...fade_in_filters];
+    if (visible_filters.length > 1) {    
+        if(button === time_button) {
+            fadeOut(name_filter_2);
+            fadeOut(name_filter_3);
+            fadeOut(name_filter_4);     
+        }
+        else if (button === setting_button) {
+            fadeOut(name_filter_3);
+            fadeOut(name_filter_4);
+        }
+        else if (button === gender_button) {
+            fadeOut(name_filter_4);
+        }  
+    }
+};
+
+// ----- when a filter list is closed, make the filters that were hidden reappear ------
+const fadeInNextFilters = () => {
+    const faded_filters = Array.from(document.getElementsByClassName('fade-out'));
+    faded_filters.forEach(filter => {
+        toggleClass(filter, 'fade-out', 'fade-in');
+    });
+};
+
 // ----- open a list of filter options ------
-const openList = (filter, button, option) => {
+const openList = (filter, button, selection) => {
     toggleClass(filter, 'list-closed', 'list-open');
     if (button.classList.contains('placeholder')) {
         toggleClass(button, 'placeholder', 'selection');
-    } else if (button.classList.contains('complete')) {
-        toggleClass(button, 'complete', 'selection');  
+    } else if (button.classList.contains('complete') && !button.classList.contains('not-allowed')) {
+        toggleClass(button, 'complete', 'selection');
+        selection.pop();
     }
-    // fadeOutAndDisableNextFilter(option);   
+    fadeOutNextFilters(button);   
 };
 
 // ----- close a list of filter options ------
 const closeList = (filter, button) => {
     toggleClass(filter, 'list-open', 'list-closed');
     toggleClass(button, 'selection', 'complete');
-    
-    if (filter === name_filter_1) {
-        scaleInNextFilter(name_filter_2);
-    } else if (filter === name_filter_2) {
-        scaleInNextFilter(name_filter_3);
-    } else if (filter === name_filter_3) {
-        scaleInNextFilter(name_filter_4);
-    }
+    fadeInNextFilters();
+};
+
+// ----- helper function, resets a completed filter back to uncompleted when a change is made to a previous filter  ------
+const resetFilter = (button, selection, text) => {
+    toggleClass(button, 'complete', 'placeholder');
+    button.textContent = text;
+    selection.pop();
+    if (button.classList.remove('not-allowed'));
 };
 
 // ----- store selected value and display inside button  ------
 const buttonHandler = (selection, eventTarget, button) => {
-    if (selection.length === 0) {
-        selection.push(eventTarget.textContent);    
-        button.textContent = selection[0];
-    } else if (selection.length !== 0 && eventTarget.textContent !== selection[0]) {
-        selection.pop();
-        selection.push(eventTarget.textContent);
-        button.textContent = selection[0];
-    };
+
+    selection.push(eventTarget.textContent);   
+    button.textContent = selection[0];
+
+    if (button === time_button && setting_button.classList.contains('complete')) {
+        resetFilter(setting_button, setting_selection, 'setting');
+    }
+    if (button === time_button  && (species_button.classList.contains('auto-complete') || species_button.classList.contains('complete')) && eventTarget.textContent !== 'the past') {
+        resetFilter(species_button, species_selection, 'species');
+        species_button.classList.remove('auto-complete');
+    }
+    if (button === setting_button && species_button.classList.contains('complete')) {
+        resetFilter(species_button, species_selection, 'species');
+    }
 };
 
 // ----- guarantee that filters with multiple possible lists only display the correct list ------
-const showOnlyOneSettingList = (selection, other1, other2) => {
+const showOnlyOneSettingList = (selection, other1, other2, other3) => {
     toggleClass(selection, 'hidden', 'visible');
     toggleClass(other1, 'visible', 'hidden');
     toggleClass(other2, 'visible', 'hidden');
+    if (other3 !== null) {
+        toggleClass(other3, 'visible', 'hidden');
+    }
 };
 
 // ----- set species to human automatically and mark as complete/disabled/highlighted  ------
 const autoSetSpeciesToHuman = () => {
     species_selection.push('human');
     species_button.textContent = 'human';
-    species_button.setAttribute('disabled', true);
-    toggleClass(species_button, 'no-highlight', 'scale-bounce-in');
-    toggleClass(species_button, 'placeholder', 'complete');
+    species_button.classList.add('not-allowed');
+    toggleClass(species_button, 'placeholder', 'auto-complete');
+    console.log('bang!')
 };
 
 // ----- time filter logic ------
 const timeHandler = () => {
     if (time_selection.includes('the past')) {
-        showOnlyOneSettingList(history_setting_selections, present_setting_selections, future_setting_selections); 
+        showOnlyOneSettingList(history_setting_selections, present_setting_selections, future_setting_selections, null); 
         gender_options[2].style.display = 'none';
         autoSetSpeciesToHuman();   
     } 
     else if (time_selection.includes('the present(-ish)')) {
-        showOnlyOneSettingList(present_setting_selections, history_setting_selections, future_setting_selections);
+        showOnlyOneSettingList(present_setting_selections, history_setting_selections, future_setting_selections, null);
     }
     else if (time_selection.includes('the future')) {
-        showOnlyOneSettingList(future_setting_selections, present_setting_selections, history_setting_selections );
+        showOnlyOneSettingList(future_setting_selections, present_setting_selections, history_setting_selections, null);
     } 
 };
 
 // ----- setting filter logic ------
 const settingHandler = () => {
     switch (setting_selection[0]) {
-        case 'on Earth as we know it':      autoSetSpeciesToHuman();                                        break;  
-        case 'in a fantasy Earth/realm':    toggleClass(fantasy_species_selections, 'hidden', 'visible');   break;      
-        case 'in space somewhere':          toggleClass(space_species_selections, 'hidden', 'visible');     break;   
-        case 'on a post-apocalyptic Earth': toggleClass(postapoc_species_selections, 'hidden', 'visible');  break;   
-        case 'on a highly advanced Earth':  toggleClass(techworld_species_selections, 'hidden', 'visible'); break; 
-    }
-};
-
-// // ----- when a filter list is opened, hide the next filter (making it 'inactive') ------
-// const fadeOutAndDisableNextFilter = (option) => {
-//     const nextFilter = Array.from(document.getElementsByClassName('no-highlight')).slice(0, 3);  
-//     nextFilter.forEach(element => {
-//         toggleClass(element, 'no-highlight', 'faded-out');
-//         if (element.classList.contains(option)) {
-//             element.setAttribute('disabled', true);
-//         }
-//     });
-// };
-
-// ----- when a filter is marked complete, highlight the next filter (making it 'active') ------
-const scaleInNextFilter = (nextFilter) => {
-    if (nextFilter !== null) {
-        toggleClass(nextFilter, 'scaled-out', 'scale-bounce-in');    
+        case 'on Earth as we know it':      
+            autoSetSpeciesToHuman();                                        
+            break;  
+        case 'in a fantasy Earth/realm':    
+            toggleClass(fantasy_species_selections, 'hidden', 'visible');   
+            showOnlyOneSettingList(fantasy_species_selections, space_species_selections, postapoc_species_selections, techworld_species_selections);
+            break;      
+        case 'in space somewhere':          
+            toggleClass(space_species_selections, 'hidden', 'visible');   
+            showOnlyOneSettingList(space_species_selections, fantasy_species_selections, postapoc_species_selections, techworld_species_selections);
+            break;   
+        case 'on a post-apocalyptic Earth': 
+            toggleClass(postapoc_species_selections, 'hidden', 'visible');  
+            showOnlyOneSettingList(postapoc_species_selections, fantasy_species_selections, space_species_selections, techworld_species_selections);
+            break;   
+        case 'on a highly advanced Earth':  
+            toggleClass(techworld_species_selections, 'hidden', 'visible'); 
+            showOnlyOneSettingList(techworld_species_selections, fantasy_species_selections, space_species_selections, postapoc_species_selections);
+            break; 
     }
 };
 
 // ----- generic function handling the return of the selected value independent of filter ------
 const returnValue = (selection, eventTarget, button, filterHandler, nextFilter) => {
-    if (selection.length === 0) {
+
+    if (selection.length === 0 || selection[0] !== eventTarget.textContent) {
+        
         buttonHandler(selection, eventTarget, button);
         filterHandler();
-    } 
-    else if (selection.length !== 0 && selection[0] !== eventTarget.textContent) {
-        selection.pop();
-        filterHandler(); 
-        buttonHandler(selection, eventTarget, button);
+
+        if (selection[0] === 'the past' && !species_selection.includes('human')) {
+            autoSetSpeciesToHuman();
+        }
     }
     scaleInNextFilter(nextFilter);
 }
@@ -216,20 +272,25 @@ const nameFilterHandler = (eventTarget) => {
         returnGenderValue(eventTarget);
     } 
     else if 
-    (eventTarget === fantasy_species_options[0] || eventTarget === fantasy_species_options[1] || eventTarget === fantasy_species_options[2] || eventTarget === fantasy_species_options[3] || eventTarget === fantasy_species_options[4] || eventTarget === fantasy_species_options[5] ||
+    ((eventTarget === fantasy_species_options[0] || eventTarget === fantasy_species_options[1] || eventTarget === fantasy_species_options[2] || eventTarget === fantasy_species_options[3] || eventTarget === fantasy_species_options[4] || eventTarget === fantasy_species_options[5] ||
      eventTarget === space_species_options[0] || eventTarget === space_species_options[1] || eventTarget === space_species_options[2] || 
      eventTarget === postapoc_species_options[0] || eventTarget === postapoc_species_options[1] || 
-     eventTarget === techworld_species_options[0] || eventTarget === techworld_species_options[1] ) {
+     eventTarget === techworld_species_options[0] || eventTarget === techworld_species_options[1]) &&  !species_button.classList.contains('not-allowed')) {
         returnSpeciesValue(eventTarget);
+    }
+    else if (eventTarget === checkmark_button) {
+        console.log(time_selection, setting_selection, gender_selection, species_selection)
     }
 
     if (time_selection.length !== 0 && setting_selection.length !== 0 && gender_selection.length !== 0 && species_selection.length !== 0) {
         toggleClass(document.getElementById('getResultsBtn'), 'filter-not-complete', 'filter-complete');
+    } 
+    else if (time_selection.length === 0 || setting_selection.length === 0 || gender_selection.length === 0 || species_selection.length === 0) {
+        toggleClass(document.getElementById('getResultsBtn'), 'filter-complete', 'filter-not-complete');
     }
 
-    
-    
-    
+    return [ time_selection, setting_selection, gender_selection, species_selection ];
+ 
 }
 
 /* ------------------------------------------------------------------------------------------------------
@@ -237,10 +298,10 @@ const nameFilterHandler = (eventTarget) => {
 ------------------------------------------------------------------------------------------------------ */
 
 // ----- filter buttons ------
-time_button.addEventListener('click', () => openList(name_filter_1, time_button, 'setting-option')); 
-setting_button.addEventListener('click', () => openList(name_filter_2, setting_button, 'gender-option')); 
-gender_button.addEventListener('click', () => openList(name_filter_3, gender_button, 'species-option'));
-species_button.addEventListener('click', () => openList(name_filter_4, species_button, null));
+time_button.addEventListener('click', () => openList(name_filter_1, time_button, time_selection)); 
+setting_button.addEventListener('click', () => openList(name_filter_2, setting_button, setting_selection)); 
+gender_button.addEventListener('click', () => openList(name_filter_3, gender_button, gender_selection));
+species_button.addEventListener('click', () => openList(name_filter_4, species_button, species_selection));
 
 // ----- filter options ------
 time_options.forEach(option => option.addEventListener('click', (e) => nameFilterHandler(e.target)));
@@ -252,3 +313,5 @@ fantasy_species_options.forEach(option => option.addEventListener('click', (e) =
 space_species_options.forEach(option => option.addEventListener('click', (e) => nameFilterHandler(e.target)));
 techworld_species_options.forEach(option => option.addEventListener('click', (e) => nameFilterHandler(e.target)));
 postapoc_species_options.forEach(option => option.addEventListener('click', (e) => nameFilterHandler(e.target)));
+
+checkmark_button.addEventListener('click', (e) => nameFilterHandler(e.target))
